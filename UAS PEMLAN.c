879c948f6a5dc9411d2cc50e77c52893e0cf35a5
Rilla-Shadina <rillashadina315@gmail.com>
2024-12-13 23:59:36 +0700

    Update UAS PEMLAN.c

--- UAS PEMLAN.c
+++ UAS PEMLAN.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+struct penerbangan {
+};
+typedef struct penerbangan penerbangan;
+
+struct penumpang {
+};
+typedef struct penumpang penumpang;
 semangat yh ges
 
